35520c20be5aa71ee4892437a9e8da3864285ab9
David Ng <davidn6597@gmail.com>
2018-05-22 01:10:44 -0400

    purged everything

--- src/components/Colors.js
+++
@@ -1,9 +0,0 @@
-export const colors = {
-  fontGrey: '#999999',
-  borderGrey: '#E3E3E3', 
-  black: '#000000',
-  white: '#ffffff',
-  cyan: '#00ffff',
-  pink: '#ff7bac',
-  lavender: '#b6a1c4',
-};
