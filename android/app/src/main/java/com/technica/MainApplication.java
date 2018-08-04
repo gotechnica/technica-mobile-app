0902c430356a56d9f8349f066b7c838af88a7d06
Tal Davidi <tal.davidi@gmail.com>
2018-08-03 23:36:58 -0700

    [notifications][android]Import notification libraries and set up firebase cloud messaging

--- android/app/src/main/java/com/technica/MainApplication.java
+++ android/app/src/main/java/com/technica/MainApplication.java
@@ -3,6 +3,11 @@ package com.technica;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.amazonaws.amplify.pushnotification.RNPushNotificationPackage;
+import com.amazonaws.amplify.pushnotification.RNPushNotificationPackage;
+import com.amazonaws.amplify.pushnotification.RNPushNotificationPackage;
+import com.amazonaws.RNAWSCognitoPackage;
+import com.amazonaws.amplify.pushnotification.RNPushNotificationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +29,11 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNPushNotificationPackage(),
+            new RNPushNotificationPackage(),
+            new RNPushNotificationPackage(),
+            new RNAWSCognitoPackage(),
+            new RNPushNotificationPackage(),
             new VectorIconsPackage()
       );
     }
