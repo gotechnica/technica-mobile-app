4ced268d0902fe8dd051c40d8c7b78517a2405c1
David Ng <davidn6597@gmail.com>
2018-09-05 16:51:05 -0400

    Update countdown timer

--- src/components/CountdownTimer.js
+++ src/components/CountdownTimer.js
@@ -3,9 +3,9 @@ import moment from 'moment';
 import { SubHeading } from './Base';
 
  // when hacking begins
-const START_TIME = moment("2018-07-07 12:00");
+const START_TIME = moment("2018-11-10 12:00");
 // when hacking ends
-const END_TIME = moment("2018-07-07 19:00");
+const END_TIME = moment("2018-11-11 19:00");
 
 export default class CountdownTimer extends Component {
   constructor(props) {
