afdb3dfed59bdd7ca6b2981dfa69f5bb4b8b9d2b
Srujan Thotapally <sthotapa@terpmail.umd.edu>
2018-09-19 03:31:05 -0400

    Finished Login.
    Features:
    	- User can enter their phone number (number only keyboard) and if the
    phone number is valid (i.e. 11 digits or more as the country code is necessary
    for twilio) a text is sent to the user.
    	- If credentials are bad, the user will see an alert saying so.
    	- User can then enter the pin.
    	- If the pin is incorrect or the time is past expiration, the user
    gets an alert saying so.
    	- If pin is correct, the app will redirect to the home page.
    	- User data is stored on the phone
    	- If the user closes and opens the app, the application checks to see
    if a user has logged on before. If so, while loading a technica 'splash
    screen' displays while the phone loads the home page.
    	- Otherwise, the login page is displayed.

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -7,7 +7,8 @@ import {
   TouchableOpacity,
   View,
   TouchableHighlight,
-  Alert
+  Alert,
+  AsyncStorage
 } from 'react-native';
 import { H1, H2, H3, H4, H6, P } from '../components/Text';
 import {
@@ -30,6 +31,9 @@ import CountdownTimer from '../components/CountdownTimer';
 import { TextInput } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/SimpleLineIcons';
 
+const EVENT_FAVORITED_STORE = 'EVENT_FAVORITED_STORE';
+const USER_DATA_STORE = 'USER_DATA_STORE';
+
 export default class Login extends Component<Props> {
   constructor(props) {
     super(props);
@@ -39,9 +43,9 @@ export default class Login extends Component<Props> {
       fieldValue: '',
       placeholder: '123-456-7890',
       greeting: 'Welcome to \nTECHNICA 2018',
-      instruction: 'Enter the phone number you used to \nsign up for Technica.',
+      instruction: 'Enter the phone number you used to \nsign up for Technica. \n\n(Include Country Code)',
       nextPage: (
-        <TouchableHighlight onPress={() => this.sendPhoneNumber(this.state.fieldValue)}>
+        <TouchableHighlight onPress={() => this.sendPhoneNumber("+" + this.state.fieldValue)}>
           <Icon
               name='arrow-right'
               size={20}
@@ -49,32 +53,131 @@ export default class Login extends Component<Props> {
               style={{alignSelf: 'flex-end', paddingRight: '5%'}}
             />
         </TouchableHighlight>),
-
+      loadPage: false,
 
     };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+/*
+    await AsyncStorage.removeItem(USER_DATA_STORE);
+    this.setState({loadPage: true});*/ 
 
+    try {
+      const value = await AsyncStorage.getItem(USER_DATA_STORE);
+      if (value !== null) {
+        const { navigate } = this.props.navigation;
+        navigate('AppContainer');
+      }
+      this.setState({loadPage: true}); 
+    } catch (error) {
+       console.log(error);
+    }
+      
   }
 
   static navigationOptions = {
     header: null,
   };
 
+  validPhoneNumber(phoneNumber){
+    var phoneRegex = RegExp('^\\+\\d{11,}$');
+    return phoneRegex.test(phoneNumber);
+  }
+
   async sendPhoneNumber(phoneNumber) {
-    let url = "https://obq8mmlhg9.execute-api.us-east-1.amazonaws.com/beta/login/check-status";
+    if(this.validPhoneNumber(phoneNumber)){
+      let url = "https://obq8mmlhg9.execute-api.us-east-1.amazonaws.com/beta/login/check-status";
+      try {
+          let response = await fetch(url, {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({'phone': phoneNumber})
+          });
+          let responseJson = await response.json();
+          if(responseJson.statusCode == 200){
+            this.setState({greeting: "Great!", instruction: "We've texted you a verification code. Please input that code below to login.", 
+            nextPage: (
+              <TouchableHighlight onPress={() => this.sendReceivedText(this.state.fieldValue)}>
+                  <Icon
+                      name='arrow-right'
+                      size={20}
+                      color='white'
+                      style={{alignSelf: 'flex-end', paddingRight: '5%'}}
+                    />
+                </TouchableHighlight>), 
+              savedPhone: phoneNumber, fieldValue: '', placeholder: 'xxxxxx'});
+          } else{
+            Alert.alert(
+              "SMS failed to send (bad credentials).",
+              "Please try again.",
+              [
+                {text: 'OK', onPress: () => console.log('OK Pressed')},
+              ],
+              { cancelable: false }
+            );
+          }
+          console.log("RESPONSE: " + JSON.stringify(responseJson));
+      } catch (error) {
+          Alert.alert(
+            "No internet connection.",
+            "Try again.",
+            [
+              {text: 'OK', onPress: () => console.log('OK Pressed')},
+            ],
+            { cancelable: false }
+          );
+        }
+    }else{
+      Alert.alert(
+        "Invalid Phone Number.",
+        "Please enter a phone number with the country code.",
+        [
+          {text: 'OK', onPress: () => console.log('OK Pressed')},
+        ],
+        { cancelable: false }
+        );
+    }
+    
+  }
+
+  async sendReceivedText(sms){
+
+    let url = "https://obq8mmlhg9.execute-api.us-east-1.amazonaws.com/beta/login/confirm-pin";
     try {
+        let phoneNumber = this.state.savedPhone;
         let response = await fetch(url, {
           method: 'POST',
           headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({'phone': phoneNumber})
+          body: JSON.stringify({'phone': phoneNumber, 'pin': sms})
         });
-        console.log("RESPONSE: " + JSON.stringify(response));
+        let responseJson = await response.json();
+        console.log("JSON:" + JSON.stringify(responseJson.body.user_data['fav_events']));
+        if(responseJson.statusCode == 200){
+          await AsyncStorage.setItem(USER_DATA_STORE, JSON.stringify(responseJson.body));
+          await AsyncStorage.setItem(EVENT_FAVORITED_STORE, JSON.stringify(responseJson.body.user_data['fav_events']));
+          this.setState({savedSMS: sms, fieldValue: ''});
+          const { navigate } = this.props.navigation;
+          navigate('AppContainer');
+        } else{
+          Alert.alert(
+            "Failed to confirm pin.",
+            "Please try again.",
+            [
+              {text: 'OK', onPress: () => console.log('OK Pressed')},
+            ],
+            { cancelable: false }
+          );
+        }
+        console.log("RESPONSE: " + JSON.stringify(responseJson));
     } catch (error) {
+        console.log(error);
         Alert.alert(
           "No internet connection.",
           "Try again.",
@@ -84,52 +187,48 @@ export default class Login extends Component<Props> {
           { cancelable: false }
         );
       }
-    
-    this.setState({greeting: "Great!", instruction: "We've texted you a verification code. Please input that code below to login.", 
-
-      nextPage: (
-      <TouchableHighlight onPress={() => this.sendReceivedText(this.state.fieldValue)}>
-          <Icon
-              name='arrow-right'
-              size={20}
-              color='white'
-              style={{alignSelf: 'flex-end', paddingRight: '5%'}}
-            />
-        </TouchableHighlight>), 
-      savedPhone: phoneNumber, fieldValue: '', placeholder: 'xxxxxx'});
-    console.log("Phone: " + this.state.savedPhone);
   }
 
-  sendReceivedText(sms){
-    this.setState({savedSMS: sms, fieldValue: ''});
-    const { navigate } = this.props.navigation;
-    navigate('AppContainer');
-  }
 
   render() {
-    return (
-      <ViewContainer>
-        <PadContainer style={styles.subSection}>
-          <Heading style={{paddingBottom: 20}}>
-            {this.state.greeting}
-          </Heading>
-          <SubHeading>
-            {this.state.instruction}
-          </SubHeading>
-          <TextInput
-            placeholder={this.state.placeholder}
-            underlineColor='white'
-            selectionColor='white'
-            value={this.state.fieldValue}
-            onChangeText={field => this.setState({ fieldValue: field })}
-            placeholderTextColor='#ffffff'
-            inputColorPlaceholder='white'
-            color='white'
-          />
-        </PadContainer>
-        {this.state.nextPage}
-      </ViewContainer>
-    );
+    if (this.state.loadPage){
+      return (
+        <ViewContainer>
+          <PadContainer style={styles.subSection}>
+            <Heading style={{paddingBottom: 20}}>
+              {this.state.greeting}
+            </Heading>
+            <SubHeading>
+              {this.state.instruction}
+            </SubHeading>
+            <TextInput
+              placeholder={this.state.placeholder}
+              underlineColor='white'
+              selectionColor='white'
+              value={this.state.fieldValue}
+              onChangeText={field => this.setState({ fieldValue: field })}
+              placeholderTextColor='#ffffff'
+              keyboardType = 'numeric'
+            />
+          </PadContainer>
+          {this.state.nextPage}
+        </ViewContainer>
+      );
+      }else{
+        return (
+        <ViewContainer>
+            <PadContainer style={styles.subSection}>
+            <Heading style={{paddingBottom: 20, paddingLeft: '23%', paddingTop: '40%'}}>
+              <Image
+                style={{width: 500, height: 100}}
+                source={require('../../assets/imgs/technica_logo.png')}
+              />
+            </Heading>
+          </PadContainer>
+        </ViewContainer>
+      );
+      }
+
   }
 }
 
