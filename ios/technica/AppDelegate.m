3801174cec729a9fe548b64f06ca937274673726
Srujan Thotapally <srujanthotapally@Srujans-MBP.fios-router.home>
2019-01-23 17:37:31 -0500

    changed files to run on react-native 0.57.8

--- ios/technica/AppDelegate.m
+++ ios/technica/AppDelegate.m
@@ -23,7 +23,11 @@
   [FIRApp configure];
   [RNFirebaseNotifications configure];
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  #ifdef DEBUG
+    jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  #else
+    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  #endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"technica"
