68c10eb9c925e8c97f67803bb1c6df57a41ec910
Srujan Thotapally <sthotapa@terpmail.umd.edu>
2019-02-25 19:45:26 -0500

    add search modal

--- src/components/SearchModal.js
+++ src/components/SearchModal.js
@@ -13,30 +13,47 @@ import {
 import Modal from 'react-native-modal';
 import { ModalContent, ModalHeader, HorizontalLine, Spacing, modalStyle } from './Base';
 import { colors } from './Colors';
-import moment from 'moment';
-import PhotoView from 'react-native-photo-view';
 import { Searchbar } from 'react-native-paper';
-import {
-  PlainViewContainer,
-  ViewContainer,
-  PadContainer,
-  Heading,
-  CenteredActivityIndicator,
-} from '../components/Base';
-import ScheduleSceneTabBar from '../components/schedule/ScheduleSceneTabBar';
-
+import EventGroupComponent from '../components/schedule/EventGroupComponent';
 
 export default class SearchModal extends Component {
+
+  constructor(props) {
+    super(props);
+    this.renderScheduleForDay = this.renderScheduleForDay.bind(this);
+  }
+
+  renderScheduleForDay(eventDayObj) {
+    eventDay = eventDayObj.item;
+    return (
+      <FlatList
+        key={eventDay.label}
+        data={eventDay.eventGroups}
+        renderItem={this.renderEventCard.bind(this)}
+        keyExtractor={(item, index) => index.toString()}
+        scrollEnabled={false}
+      />
+    );
+  }
+
+  renderEventCard(eventGroupObj) {
+    eventGroup = eventGroupObj.item;
+    return (
+      <EventGroupComponent
+        header={eventGroup.label}
+        events={eventGroup.events}
+        eventManager={this.props.eventManager}
+      />
+    );
+  }
+
   render() {
     const props = this.props;
     const dimensions = require('Dimensions').get('window');
     const imageWidth = dimensions.width - 42;
     const imageHeight = Math.round((imageWidth * 38) / 67);
     const styles = {width: window.width, height: window.height, overflow:'visible'};
-    console.log(props);
     eventDays = props.eventDays;
-    console.log("DAYs: " + eventDays);
-    //let tabNames = eventDays.map(eventDay => eventDay.label);
     return (
       <Modal
         isVisible={props.isModalVisible}
@@ -65,47 +82,6 @@ export default class SearchModal extends Component {
           <FlatList
             data={eventDays}
             renderItem={this.renderScheduleForDay}
-            ListHeaderComponent={() => (
-              <PadContainer>
-                <ScheduleSceneTabBar
-                  goToSection={i => {
-                    this.scheduleListRef.scrollToIndex({
-                      index: i,
-                      viewOffset: 100,
-                      viewPosition: 0
-                    });
-                  }}
-                  tabs={tabNames}
-                  activeTab={0}
-                />
-              </PadContainer>
-            )}
-            ItemSeparatorComponent={() => (
-              <PadContainer
-                style={{
-                  marginTop: 20,
-                  borderTopWidth: 1,
-                  borderColor: colors.borderColor.light,
-                  paddingTop: 30,
-                }}
-              >
-                <ScheduleSceneTabBar
-                  goToSection={i => {
-                    this.scheduleListRef.scrollToIndex({
-                      index: i,
-                      viewOffset: 100,
-                      viewPosition: 0
-                    });
-                  }}
-                  tabs={tabNames}
-                  activeTab={1}
-                />
-              </PadContainer>
-            )}
-            keyExtractor={(eventDay, index) => eventDay.label}
-            ref={ref => {
-              this.scheduleListRef = ref;
-            }}
           />
         </ModalContent>
       </Modal>
