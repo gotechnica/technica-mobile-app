85008e6f882f17779f8e3cc160fc3d3385e1a53f
yreiss1 <reiss.yuval@gmail.com>
2019-07-29 01:34:07 -0400

    Added comments to Saved and Schedule screens

--- src/screens/Schedule.js
+++ src/screens/Schedule.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Platform,
   StyleSheet,
@@ -6,20 +6,23 @@ import {
   FlatList,
   View,
   ActivityIndicator
-} from 'react-native';
-import { H1, H2, H3, H4, P } from '../components/Text';
+} from "react-native";
+import { H1, H2, H3, H4, P } from "../components/Text";
 import {
   PlainViewContainer,
   ViewContainer,
   PadContainer,
   Heading,
-  CenteredActivityIndicator,
-} from '../components/Base';
-import ScrollableTabView from 'react-native-scrollable-tab-view';
-import EventGroupComponent from '../components/schedule/EventGroupComponent';
-import ScheduleSceneTabBar from '../components/schedule/ScheduleSceneTabBar';
-import { colors } from '../components/Colors';
+  CenteredActivityIndicator
+} from "../components/Base";
+import ScrollableTabView from "react-native-scrollable-tab-view";
+import EventGroupComponent from "../components/schedule/EventGroupComponent";
+import ScheduleSceneTabBar from "../components/schedule/ScheduleSceneTabBar";
+import { colors } from "../components/Colors";
 
+/**
+ * Schedule page lists the schedule of events for all days of the hackathon.
+ */
 export default class Schedule extends Component<Props> {
   constructor(props) {
     super(props);
@@ -28,7 +31,10 @@ export default class Schedule extends Component<Props> {
   }
 
   componentDidMount() {}
-
+  /**
+   * description: Function returns a list containing all hackathon events within a day.
+   * @param {Object} eventDayObj object that contains the event day to be displayed.
+   */
   renderScheduleForDay(eventDayObj) {
     eventDay = eventDayObj.item;
     return (
@@ -41,7 +47,10 @@ export default class Schedule extends Component<Props> {
       />
     );
   }
-
+  /**
+   * description: Returns a component representing a specific hackathon event.
+   * @param {Object} eventGroupObj object that contains information regarding the event.
+   */
   renderEventCard(eventGroupObj) {
     eventGroup = eventGroupObj.item;
     return (
@@ -57,19 +66,20 @@ export default class Schedule extends Component<Props> {
     let eventDays = this.props.eventManager.getEventDays();
     let tabNames = eventDays.map(eventDay => eventDay.label);
 
-    if(eventDays.length == 0) {
-      return (
-        <CenteredActivityIndicator/>
-      )
+    if (eventDays.length == 0) {
+      /* UI Spinner which indicates that the app is loading data */
+      return <CenteredActivityIndicator />;
     } else {
       return (
         <PlainViewContainer>
+          {/* A list that contains all events for the day */}
           <FlatList
             data={eventDays}
             renderItem={this.renderScheduleForDay}
             ListHeaderComponent={() => (
               <PadContainer>
                 <Heading>Schedule</Heading>
+                {/* TabBar which displays the days of the hackathon, 1st day is highlighted */}
                 <ScheduleSceneTabBar
                   goToSection={i => {
                     this.scheduleListRef.scrollToIndex({
@@ -83,15 +93,18 @@ export default class Schedule extends Component<Props> {
                 />
               </PadContainer>
             )}
+            /* Divider which splits the event lists of the different days of the hackathon */
+
             ItemSeparatorComponent={() => (
               <PadContainer
                 style={{
                   marginTop: 20,
                   borderTopWidth: 1,
                   borderColor: colors.borderGrey,
-                  paddingTop: 30,
+                  paddingTop: 30
                 }}
               >
+                {/* TabBar which displays the days of the hackathon, 2nd day is highlighted */}
                 <ScheduleSceneTabBar
                   goToSection={i => {
                     this.scheduleListRef.scrollToIndex({
