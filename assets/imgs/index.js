1f22262786303bd3beb24050d1e3921a57c7da36
Ravi Kemburu <ravikemburu@lyft.com>
2019-10-30 15:25:12 -0400

    Add connect page
    Add connect with us page to app bar that contains social media hyperlinks

--- assets/imgs/index.js
+++ assets/imgs/index.js
@@ -16,6 +16,13 @@ const Images = {
     banner_demos_big: require('./Big_Banner06_Demos.png'),
     banner_sponsorship_big: require('./Big_Banner07_Sponsorship.png'),
     banner_fun_big: require('./Big_Banner08_FunCelebrate.png'),
+
+    facebook: require('./facebook.png'),
+    insta: require('./insta.png'),
+    linkedin: require('./linkedin.png'),
+    medium: require('./medium.png'),
+    twitter: require('./twitter.png'),
+    youtube: require('./youtube.png'),
 };
 
 export default Images;
